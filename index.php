e7c7f8985da95357c422adddef02504c34349517
Saffat Bokul <saffatbokul@gmail.com>
2019-10-20 12:53:30 +0600

    login page fix

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
-    <?php
-    include('login.php'); 
-    if(isset($_SESSION['login_user'])){
-    header("location: profile.php"); 
-    }
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Login Form in PHP with Session</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-    <div id="login">
-    <h2>Login Form</h2>
-    <form action="" method="post">
-    <label>UserName :</label>
-    <input id="name" name="username" placeholder="username" type="text">
-    <label>Password :</label>
-    <input id="password" name="password" placeholder="**********" type="password"><br><br>
-    <input name="submit" type="submit" value=" Login ">
-    <span><?php echo $error; ?></span>
-    </form>
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
-    </body>
-    </html>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
+// Initialize the session
 session_start();
-if(session_destroy()) {
-header("Location: index.php"); 
-}
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
 ?>
